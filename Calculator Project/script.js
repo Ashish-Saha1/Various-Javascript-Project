aa6fbf753646feb7c1e35c809b66840f9056daed
Ashish Saha <rubelsaha1@gmail.com>
2024-01-20 08:46:30 +0600

    Calculator project css complete from home

---
+++ Calculator Project/script.js
@@ -0,0 +1,10 @@
+let inputBox = document.getElementById('input-box');
+let operators = document.querySelectorAll('.operator');
+let buttons = document.querySelectorAll('button')
+
+
+console.log(buttons)
+let str = '';
+buttons.forEach(item=>{
+    console.dir(item.innerHTML)
+})
