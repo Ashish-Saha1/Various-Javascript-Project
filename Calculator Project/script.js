828cac4b093139bf2129e6c75f04d2a92a9c67d5
ashish.saha <rubelsaha1@gmail.com>
2024-01-20 14:47:21 +0600

    Calulator project complete & fix bug from Office

--- Calculator Project/script.js
+++ Calculator Project/script.js
@@ -1,10 +1,42 @@
 let inputBox = document.getElementById('input-box');
-let operators = document.querySelectorAll('.operator');
 let buttons = document.querySelectorAll('button')
 
 
-console.log(buttons)
-let str = '';
-buttons.forEach(item=>{
-    console.dir(item.innerHTML)
+
+let str = "";
+
+
+
+buttons.forEach(button=>{
+    button.addEventListener('click', function(event){
+       if(inputBox.value === "" && event.target.innerHTML == "="){
+            str = "";
+            inputBox.value = str;
+       }
+        else if(event.target.innerHTML == "="){
+            str = eval(str);           
+            inputBox.value = str;
+            console.log(typeof str);
+             
+        }else if(event.target.innerHTML === "AC"){
+            str = "";
+            inputBox.value = str;
+        }else if(event.target.innerHTML === "Del"){
+            console.log(typeof str);
+            str = str.toString()
+            str = str.substring(0, str.length-1)
+            inputBox.value = str;
+            console.log(typeof str);
+        }
+
+        else{
+           str += event.target.innerHTML;
+           inputBox.value = str;
+           console.log(typeof str);
+        }
+    })
 })
+
+
+
+
