1c6c6fd2593403d97c98ed7c6fa67310f54586de
ashish.saha <rubelsaha1@gmail.com>
2024-01-25 17:47:48 +0600

    Calculator project -2 try-3 & Added New Code to fix bug from Office

--- Calculator Project- 2/script.js
+++ Calculator Project- 2/script.js
@@ -1,140 +1,182 @@
-// let inputBox = document.getElementById('input-box');
-// let buttons = document.querySelectorAll('button')
 
 
 
-// let str = "";
+// let firstNumber;
+// let lastNumber;
+// let step = 0;
+// let result = 0;
+// let operation;
+
+// let firstArr = [];
+// let secondArr = [];
 
 
-
-// buttons.forEach(button=>{
-//     button.addEventListener('click', function(event){
-//        if(inputBox.value === "" && event.target.innerHTML == "="){
-//             str = "";
-//             inputBox.value = str;
-//        }
-//         else if(event.target.innerHTML === "="){
-//             str = eval(str);           
-//             inputBox.value = str;
-//             console.log(typeof str);
-             
-//         }else if(event.target.innerHTML === "AC"){
-//             str = "";
-//             inputBox.value = str;
-//         }else if(event.target.innerHTML === "Del"){
-//             console.log(typeof str);
-//             str = str.toString()
-//             str = str.substring(0, str.length-1)
-//             inputBox.value = str;
-//             console.log(typeof str);
-//         }
-
-//         else{
-//            str += event.target.innerHTML;
-//            inputBox.value = str;
-//            console.log(typeof str);
-//         }
-//     })
-// })
-
-
-let firstNumber;
-let lastNumber;
-let step = 0;
-let result = 0;
-let operation;
-
-let firstArr = [];
-let secondArr = [];
-
-
-let display = document.querySelector('#input-box')
+// let display = document.querySelector('#input-box')
 
 
 
 
 
 
-function getNum(num){
-    if(step === 0 || step === 1){
+// function getNum(num){
+//     if(step === 0 || step === 1){
 
-        firstArr.push(num);
-        step = 1;
-        firstNumber = Number(firstArr.join(''))
-        display.value = firstNumber
+//         firstArr.push(num);
+//         step = 1;
+//         firstNumber = Number(firstArr.join(''))
+//         display.value = firstNumber
      
-    }else if(step === 2){
+//     }else if(step === 2){
         
-        secondArr.push(num);
-        lastNumber = Number(secondArr.join(''));
-        display.value = lastNumber;
+//         secondArr.push(num);
+//         lastNumber = Number(secondArr.join(''));
+//         display.value = lastNumber;
 
        
-        console.log(lastNumber)
-        
-    }
-    else if(step === 3){
-        lastNumber = null
-        console.log(step)
-    }
+//         console.log(lastNumber)
+       
+//     }
+//     else if(step === 3){
+//         lastNumber = null
+//         console.log(step)
+//     }
 
+   
+
+// }
+
+
+// function getOperator(operator){
+//     step = 2;
+//     console.log(step)
+//     operation = operator;
+
+// }
+
+
+
+
+// function calculateFun(){
+ 
+//     console.log(step)
+//     if(operation === "+"){
+        
+//         result = firstNumber + lastNumber;
+//         display.value = result;
+        
+    
+//     }else if(operation === "-"){
+//         result = firstNumber - lastNumber;
+//         display.value = result;
+//     }else if(operation === "*"){
+//         result = firstNumber * lastNumber;
+//         display.value = result;
+//     }else if(operation === "/"){
+//         result = firstNumber / lastNumber;
+//         display.value = result;
+//     }
+
+//     // secondArr = []
+//     // lastNumber = null;
+//     //firstNumber = result;
+//     // console.log(result)
+//     // console.log(firstNumber)
+//     // console.log(lastNumber)
+     
+// }
+
+
+// function clearAll(){
+   
+    
+//     display.value = 0;
+//     firstNumber = null;
+//     lastNumber = null;
+//     step = 0;
+//     result = 0;
+//     operation = null;
+//     firstArr = [];
+//     secondArr = [];
+
+// }
+
+
+
+
+
+
+let display = document.querySelector('#input-box');
+
+console.log(display.value)
+
+function getNum(num){
+display.value+=num;
    
 
 }
 
+function calculateFun(){
+let result;
+let inputArray = [];
 
-function getOperator(operator){
-    step = 2;
-    console.log(step)
-    operation = operator;
+
+
+
+
+
+
+
+    if(display.value.includes('+')){
+        inputArray = display.value.split('+');
+        display.value =  parseInt(inputArray[0]) + parseInt(inputArray[1])
+        
+    }else if(display.value.includes('-')){
+        inputArray = display.value.split('-');
+        display.value =  parseInt(inputArray[0]) - parseInt(inputArray[1])
+    }else if(display.value.includes('*')){
+        inputArray = display.value.split('*');
+        display.value =  parseInt(inputArray[0]) * parseInt(inputArray[1])
+    }else if(display.value.includes('/')){
+        inputArray = display.value.split('/');
+        display.value =  parseInt(inputArray[0]) / parseInt(inputArray[1])
+    }else if(display.value.includes('%')){
+        inputArray = display.value.split('%');
+        display.value =  parseInt(inputArray[0]) - parseInt(inputArray[1])
+    }else{
+        display.value = ""
+    }
+
+
 
 }
 
-
-
-
-function calculateFun(){
- 
-    console.log(step)
-    if(operation === "+"){
-        
-        result = firstNumber + lastNumber;
-        display.value = result;
+function deleteNum(){
+    let convertString = display.value.toString();
+   let deleteRightSideValue = convertString.slice(0, convertString.length -1)
+    display.value = deleteRightSideValue
     
-    }else if(operation === "-"){
-        result = firstNumber - lastNumber;
-        display.value = result;
-    }else if(operation === "*"){
-        result = firstNumber * lastNumber;
-        display.value = result;
-    }else if(operation === "/"){
-        result = firstNumber / lastNumber;
-        display.value = result;
-    }
-
-    // secondArr = []
-    // lastNumber = null;
-    //firstNumber = result;
-    // console.log(result)
-    // console.log(firstNumber)
-    // console.log(lastNumber)
-     
 }
 
 
 function clearAll(){
-   
-    
-    display.value = 0;
-    firstNumber = null;
-    lastNumber = null;
-    step = 0;
-    result = 0;
-    operation = null;
-    firstArr = [];
-    secondArr = [];
-
+    display.value = ""
 }
 
 
 
+
+
+
+
+ 
+//  x.addEventListener('input', function(e){
+//  let x = e.target.value;
+// let reg = /[^a-z]/ig
+   
+// console.log(reg.test(x));
+//     if(reg.test(x)){
+
+//     }
+
+   
+//  })
