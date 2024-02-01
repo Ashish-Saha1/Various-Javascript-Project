4f5f950f863c5a9b63f325191ec49ee6d23db163
ashish.saha <rubelsaha1@gmail.com>
2024-02-01 17:03:33 +0600

    Custom Digital clock completed from office

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,4 +1,4 @@
-let display = document.querySelector('p')
+// let display = document.querySelector('p')
 // function time(){
 //     const d = new Date()
 //     const hour = new Date().getHours();
@@ -45,15 +45,45 @@ let display = document.querySelector('p')
 // }, 1000);
 
 
-let date = new Date();
+// let date = new Date();
    
-let times = date.toLocaleString('en-US', { hour: '2-digit', hour12: true, minute: '2-digit', second: 'numeric' })
+// let times = date.toLocaleString('en-US', { hour: '2-digit', hour12: true, minute: '2-digit', second: 'numeric' })
 
-let day = date.getDate()
-let month = date.getMonth()+1
-let year = date.getFullYear()
+// let day = date.getDate()
+// let month = date.getMonth()+1
+// let year = date.getFullYear()
 
 
-let da = `${day}/${month}/${year}`
-console.log(year)
-display.innerHTML =  times
+// let da = `${day}/${month}/${year}`
+// console.log(year)
+// display.innerHTML =  times
+
+
+
+// To make a digital clock manualy or customed
+let displayHour = document.querySelector('.hour');
+let displayMin = document.querySelector('.min');
+let displaySec = document.querySelector('.sec');
+let displayAmPm = document.querySelector('.am-pm');
+let displayDate = document.querySelector('.show-date');
+function clock(){
+    let dateObject = new Date();
+    let hour = dateObject.getHours();
+    let min = dateObject.getMinutes();
+    let sec = dateObject.getSeconds();
+    let amPm = hour >= 12? "PM":"AM";
+    min = min < 10? "0"+min:min;
+    sec = sec < 10? "0"+sec:sec;
+    hour = hour % 12;
+
+    let currentDate = dateObject.toDateString()
+
+    displayHour.innerHTML = hour;
+    displayMin.innerHTML = min;
+    displaySec.innerHTML = sec;
+    displayAmPm.innerHTML = amPm;
+    displayDate.innerHTML = currentDate
+}
+
+
+setInterval(clock,1000)
