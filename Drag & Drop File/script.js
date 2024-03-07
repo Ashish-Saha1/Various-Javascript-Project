e921d2fd5939c111779c0155a7ca3a6c1fea97bb
Ashish Saha <rubelsaha1@gmail.com>
2024-03-07 23:54:28 +0600

    Drag & drop project is working 1st commit from home

---
+++ Drag & Drop File/script.js
@@ -0,0 +1,10 @@
+let browseFileBtn = document.querySelector('.container button');
+let inputFile = document.querySelector('input');
+
+
+browseFileBtn.addEventListener('click',()=>{
+    browseFileBtn.style.color = 'red';
+    inputFile.click()
+
+
+})
