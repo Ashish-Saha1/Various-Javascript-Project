ddb97342a45de498d6cfdddb8094ddd8f697aafb
Ashish Saha <rubelsaha1@gmail.com>
2024-03-08 23:37:15 +0600

    Drag & drop project is working with drop an image from home

--- Drag & Drop File/script.js
+++ Drag & Drop File/script.js
@@ -1,5 +1,6 @@
 let browseFileBtn = document.querySelector('.container button');
 let inputFile = document.querySelector('input');
+let mainContainer = document.querySelector('.container')
 
 
 browseFileBtn.addEventListener('click',()=>{
@@ -7,4 +8,40 @@ browseFileBtn.addEventListener('click',()=>{
     inputFile.click()
 
 
+})
+
+inputFile.addEventListener('change', function(event){
+        let file = event.target.files[0];
+            urlLink = URL.createObjectURL(file)
+            mainContainer.style.background = `URL(${urlLink})`;
+          for(let i of mainContainer.children){
+                i.style.display = 'none'
+          }
+})
+
+mainContainer.addEventListener('dragover', function(event){
+    let dragText = document.querySelector('.drag-text');
+    dragText.textContent = "Realese to upload";
+    mainContainer.style.border = 'solid'
+    mainContainer.style.borderColor = 'rgb(201, 203, 204)'
+
+
+})
+
+mainContainer.addEventListener('dragleave', function(event){
+    let dragText = document.querySelector('.drag-text');
+    dragText.textContent = "Drag & Drop";
+    mainContainer.style.border = 'dotted';
+    mainContainer.style.borderColor = 'rgb(201, 203, 204)'
+    
+})
+
+mainContainer.addEventListener('drop', function(event){
+    event.preventDefault()
+    let dragText = document.querySelector('.drag-text');
+    dragText.textContent = "Drag & Drop";
+    let file = event.dataTransfer.files[0];
+    console.dir(file)
+    urlLink = URL.createObjectURL(file)
+    mainContainer.style.background = `URL(${urlLink})`;
 })
